7b0fbae1be31f65ac2ffa2091e0c49a9198bba2a
punyakriti5 <83115236+punyakriti5@users.noreply.github.com>
2024-01-13 15:45:53 +0530

    tagline

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-   <h1>This is Resolvia UI</h1>
+    <>
+   <h1>Welcome to Resolvia</h1>
+   <h2> a community where connectivity meets innovation </h2>
+   </>
   );
 }
 
