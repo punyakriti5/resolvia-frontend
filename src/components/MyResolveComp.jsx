4e1ed8be44d39516ec1e9cf8b990d03f15f5c1c5
Abishek sridhar <abishekcs3896@gmail.com>
2024-02-13 17:34:24 +0530

    'search and sort frontend'

--- src/components/MyResolveComp.jsx
+++ src/components/MyResolveComp.jsx
@@ -9,19 +9,20 @@ import {
   InputBase,
   Typography,
   Stack,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import DrawIcon from "@mui/icons-material/Draw";
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import DrawIcon from '@mui/icons-material/Draw';
 import myResolve from '../assets/myResolvee.jpg';
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function MyResolveComp() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector(state => state.user);
   const [userResolves, setUserResolves] = useState([]);
-  const [errorMessage,setErrorMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [mySearch, setMySearch] = useState('');
   const [loading, setLoading] = useState(true);
   const [showMore, setShowMore] = useState(true);
 
@@ -30,7 +31,9 @@ function MyResolveComp() {
       try {
         setLoading(true);
         const res = await fetch(
-          `/api/resolve/getresolves?userId=${currentUser && currentUser._id}`
+          `/api/resolve/getresolves?userId=${
+            currentUser && currentUser._id
+          }&searchTerm=${mySearch}`
         );
         console.log('response my resolve:', res);
         const data = await res.json();
@@ -47,22 +50,24 @@ function MyResolveComp() {
         setErrorMessage(error.message);
       }
     };
-    
-    fetchResolves();
-  }, []);
-  console.log("userResolves", userResolves)
 
+    fetchResolves();
+  }, [mySearch]);
+  //console.log("userResolves", userResolves)
+  const handleSearch = e => {
+    setMySearch(e.target.value);
+  };
   const handleShowMore = async () => {
     const startIndex = userResolves.length;
     try {
       setLoading(true);
       const res = await fetch(
-        `/api/post/getresolves?userId=${currentUser._id}&startIndex=${startIndex}`
+        `/api/post/getresolves?userId=${currentUser._id}&startIndex=${startIndex}&searchTerm=${mySearch}`
       );
       const data = await res.json();
       if (res.ok) {
         setLoading(false);
-        setUserResolves((prev) => [...prev, ...data.resolves]);
+        setUserResolves(prev => [...prev, ...data.resolves]);
         if (data.resolves.length < 9) {
           setShowMore(false);
         }
@@ -76,53 +81,64 @@ function MyResolveComp() {
   return (
     <>
       <Container sx={{ mt: 10 }}>
-     
         <Stack spacing={2}>
           <Typography
-            variant="body1"
-            color="#034f84"
+            variant='body1'
+            color='#034f84'
             sx={{
-              textAlign: "center",
-              textTransform: "capitalize",
-              fontWeight: "bold",
+              textAlign: 'center',
+              textTransform: 'capitalize',
+              fontWeight: 'bold',
               borderBottom: 1.5,
-              borderColor: "#034f84",
+              borderColor: '#034f84',
             }}
           >
             My Resolves
           </Typography>
           {currentUser && userResolves.length > 0 ? (
-            <Button variant="outlined" endIcon={<SearchIcon />}>
-              <InputBase placeholder=" My resolves" />
+            <Button variant='outlined' endIcon={<SearchIcon />}>
+              <InputBase
+                placeholder=' My resolves'
+                value={mySearch}
+                onChange={handleSearch}
+              />
             </Button>
           ) : (
             <>
-            <Stack spacing={2} direction="row">
-            <Typography variant="body1"  sx={{
-              textAlign: "center",
-              fontWeight: "medium",}}>Not created any resolve yet?</Typography>
-            <Link to="/create-resolve">
-            <Button variant="contained"  size="small" sx={{ backgroundColor: "#034f84",
-        width: '90%', }} startIcon={<DrawIcon />}>
-      Create Resolve
-    </Button>
+              <Stack spacing={2} direction='row'>
+                <Typography
+                  variant='body1'
+                  sx={{
+                    textAlign: 'center',
+                    fontWeight: 'medium',
+                  }}
+                >
+                  Not created any resolve yet?
+                </Typography>
+                <Link to='/create-resolve'>
+                  <Button
+                    variant='contained'
+                    size='small'
+                    sx={{ backgroundColor: '#034f84', width: '90%' }}
+                    startIcon={<DrawIcon />}
+                  >
+                    Create Resolve
+                  </Button>
                 </Link>
-                </Stack>
-            <Box
-            component='img'
-            sx={{
-              alignSelf:"center",
-              width: '320px',
-              height: '420px',
-            }}
-            alt='create my resolves'
-            src={myResolve}
-          />
-        
+              </Stack>
+              <Box
+                component='img'
+                sx={{
+                  alignSelf: 'center',
+                  width: '320px',
+                  height: '420px',
+                }}
+                alt='create my resolves'
+                src={myResolve}
+              />
             </>
-            
           )}
-          {userResolves.map((resolve) => (
+          {userResolves.map(resolve => (
             <Accordion key={resolve.slug}>
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                 {resolve.title}
@@ -130,7 +146,7 @@ function MyResolveComp() {
               <AccordionDetails>{resolve.content}</AccordionDetails>
               <Link to={`/resolve/${resolve.slug}`}>
                 <AccordionActions>
-                  <Button variant="contained" sx={{ height: 30 }}>
+                  <Button variant='contained' sx={{ height: 30 }}>
                     learn more
                   </Button>
                 </AccordionActions>
--- src/pages/User_Dashboard.jsx
+++ src/pages/User_Dashboard.jsx
@@ -1,38 +1,39 @@
-import React,{useState,useEffect} from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
-import { Box, Container, Grid, useMediaQuery } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import Navbar from "../components/Navbar";
-import CardComp from "../components/CardComp";
-import MyResolveComp from "../components/MyResolveComp";
-import SortingComp from "../components/SortingComp";
-
+import { Box, Container, Grid, useMediaQuery } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navbar from '../components/Navbar';
+import CardComp from '../components/CardComp';
+import MyResolveComp from '../components/MyResolveComp';
+import SortingComp from '../components/SortingComp';
 function User_Dashboard() {
-  const matches = useMediaQuery("(min-width:960px)");
-  const {userId} = useParams();
-  const { currentUser } = useSelector((state) => state.user);
+  const matches = useMediaQuery('(min-width:960px)');
+  const { userId } = useParams();
+  const { currentUser } = useSelector(state => state.user);
   const [feedResolve, setFeedResolve] = useState([]);
   const navigate = useNavigate();
-  const [errorFeed,setErrorFeed] = useState(false);
+  const [errorFeed, setErrorFeed] = useState(false);
   const [loading, setLoading] = useState(true);
   const [showMore, setShowMore] = useState(true);
-
-
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('latest');
+  const [sortOrder, setSortOrder] = useState('desc');
+  const location = useLocation();
   useEffect(() => {
     const fetchAllResolve = async () => {
       try {
         setLoading(true);
         const res = await fetch(
-          `/api/resolve/getresolves`
+          `/api/resolve/getresolves?sortBy=${sortBy}&sortOrder=${sortOrder}&searchTerm=${searchTerm}`
         );
         const data = await res.json();
         if (res.ok) {
           setLoading(false);
           setFeedResolve(data.resolves);
-          console.log("feedResolve", feedResolve)
+          //console.log('feedResolve', feedResolve);
           if (data.feedResolve.length < 10) {
             setShowMore(false);
           }
@@ -44,9 +45,22 @@ function User_Dashboard() {
     };
 
     fetchAllResolve();
-  }, []);
-
-  const handleLike = async (resolveId) => {
+  }, [sortBy, sortOrder, searchTerm]);
+  const handleSearch = e => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSortBy = e => {
+    setSortBy(e.target.value);
+  };
+  const handleSortOrder = () => {
+    // console.log('Clicking this button');
+    if (sortOrder === 'desc') {
+      setSortOrder('asc');
+    } else {
+      setSortOrder('desc');
+    }
+  };
+  const handleLike = async resolveId => {
     try {
       if (!currentUser) {
         navigate('/login');
@@ -65,7 +79,7 @@ function User_Dashboard() {
 
       if (res.ok) {
         setFeedResolve(
-          feedResolve.map((resolve) =>
+          feedResolve.map(resolve =>
             resolve._id === resolveId
               ? {
                   ...resolve,
@@ -81,54 +95,58 @@ function User_Dashboard() {
     }
   };
 
-
-  console.log("feedResolve", feedResolve)
+  console.log('feedResolve', feedResolve);
   return (
-    <> 
-    <Box sx={{ display: "flex" }}>
-    <CssBaseline />
-    <Navbar />
-    <Box
-      sx={{
-       
-        maxWidth: 450,
-        minWidth: 350,
-        width:"45vw",
-       
-        position: "sticky",
-        top: 0, 
-        overflowY: "auto",
-        boxShadow: 3,
-      }}
-    >
-      <MyResolveComp />
-    </Box>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        p: 3,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={4}>
-            <SortingComp />
-          </Grid>
-          <Grid item xs={12}>
-          {feedResolve.map((resolve) => (
-            <CardComp   key={resolve._id}
-            resolve={resolve} onLike={handleLike}/>
-            ))}
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </Box>
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar searchTerm={searchTerm} handleSearch={handleSearch} />
+        <Box
+          sx={{
+            maxWidth: 450,
+            minWidth: 350,
+            width: '45vw',
 
+            position: 'sticky',
+            top: 0,
+            overflowY: 'auto',
+            boxShadow: 3,
+          }}
+        >
+          <MyResolveComp />
+        </Box>
+        <Box
+          component='main'
+          sx={{
+            flexGrow: 1,
+            p: 3,
+          }}
+        >
+          <Container maxWidth='lg'>
+            <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              <Grid item xs={4}>
+                <SortingComp
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  handleSortBy={handleSortBy}
+                  handleSortOrder={handleSortOrder}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                {feedResolve.map(resolve => (
+                  <CardComp
+                    key={resolve._id}
+                    resolve={resolve}
+                    onLike={handleLike}
+                  />
+                ))}
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
     </>
   );
 }
 
 export default User_Dashboard;
-
