72f78b6492059a10b10d6702e0c92a74e61538a5
Abishek sridhar <abishekcs3896@gmail.com>
2024-02-14 23:20:17 +0530

    'Update Profile'

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -1,106 +1,124 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Avatar,
   Box,
   Button,
   Chip,
+  Input,
+  Stack,
   Grid,
+  IconButton,
   Paper,
   TextField,
   Typography,
-} from "@mui/material";
-import { blue } from "@mui/material/colors";
-import Navbar from "../components/Navbar";
-
-import { useDispatch, useSelector } from "react-redux";
+} from '@mui/material';
+import { blue } from '@mui/material/colors';
+import Navbar from '../components/Navbar';
+import EditIcon from '@mui/icons-material/Edit';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   updateStart,
   updateSuccess,
   updateFailure,
-} from "../features/user/userSlice";
-import dataTags from "../data/tags.json";
-
+} from '../features/user/userSlice';
+import dataTags from '../data/tags.json';
 
 function UpdateProfile() {
   const {
     currentUser,
     loading,
     error: errorMessage,
-  } = useSelector((state) => state.user);
+  } = useSelector(state => state.user);
   const [profileData, setProfileData] = useState({});
-
+  const [profilePhoto, setProfilePhoto] = useState(null);
   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
   const [updateUserError, setUpdateUserError] = useState(null);
 
   const [selectedTags, setSelectedTags] = useState([]);
   const dispatch = useDispatch();
 
- 
- 
- useEffect(() => {
-   const getProfileData = async () => {
-     try {
-       const res = await fetch(`/api/user/getUser/${currentUser._id}`);
-       const data = await res.json();
-       if (res.ok) {
-         setProfileData(data);
-       }
-     } catch (error) {
-       console.log(error.message);
-     }
-   };
-   getProfileData();
- }, []);
+  useEffect(() => {
+    const getProfileData = async () => {
+      try {
+        const res = await fetch(`/api/user/getUser/${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setProfileData(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getProfileData();
+  }, []);
 
+  useEffect(() => {
+    const getProfilePhotoUrl = async () => {
+      const form = new FormData();
+      form.append('photo', profilePhoto);
+      try {
+        const res = await fetch(`/api/user/uploadPhoto`, {
+          method: 'POST',
+          body: form,
+        });
+        const data = await res.json();
+        if (res.ok) {
+          setProfileData(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getProfilePhotoUrl();
+  }, [profilePhoto]);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setProfileData({
-      
       ...profileData,
       [e.target.id]: e.target.value,
     });
   };
 
-  const handleClick = (tag) => {
-    
+  const handleClick = tag => {
     const selectedIndex = selectedTags.indexOf(tag);
     let newSelectedTags = [];
-  
+
     if (selectedIndex === -1) {
       newSelectedTags = [...selectedTags, tag];
     } else {
-      newSelectedTags = [...selectedTags.slice(0, selectedIndex), ...selectedTags.slice(selectedIndex + 1)];
+      newSelectedTags = [
+        ...selectedTags.slice(0, selectedIndex),
+        ...selectedTags.slice(selectedIndex + 1),
+      ];
     }
-  
+
     setSelectedTags(newSelectedTags);
-    setProfileData({...profileData,
-      category: newSelectedTags,
-  });
-    console.log("profiledata after click",profileData);
+    setProfileData({ ...profileData, category: newSelectedTags });
+    console.log('profiledata after click', profileData);
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     setUpdateUserError(null);
     setUpdateUserSuccess(null);
     if (Object.keys(profileData).length === 0) {
-      setUpdateUserError("No changes made");
+      setUpdateUserError('No changes made');
       return;
     }
 
     try {
       dispatch(updateStart());
-      console.log("update starting...")
+      console.log('update starting...');
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: "PUT",
+        method: 'PUT',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
 
         body: JSON.stringify(profileData),
       });
       const data = await res.json();
-      console.log("data", data);
+      console.log('data', data);
       if (!res.ok) {
         dispatch(updateFailure(data.message));
         setUpdateUserError(data.message);
@@ -119,26 +137,46 @@ function UpdateProfile() {
       <Navbar />
       <Grid container spacing={2} sx={{ mt: 8 }}>
         <Grid item xs={12} md={4} lg={4}>
-          <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-          <Avatar
-              sx={{
-                bgcolor: blue[100],
-                height: "120px",
-                width: "120px",
-                mt: 1,
-              }}
-              aria-label="resolve"
-            >
-              {profileData && (
-                <img
-                  src={profileData.profilePicture}
-                  alt="profile-picture"
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
-              )}
-            </Avatar>
-            <Typography sx={{ fontWeight: "medium", mt: 2 }}>
-              {profileData && profileData.username}
+          <Box display='flex' flexDirection='column' alignItems='center' p={2}>
+            <Input
+              accept='image/*'
+              style={{ display: 'none' }}
+              id='profilePicture'
+              type='file'
+              onChange={e => setProfilePhoto(e.target.files[0])}
+            />
+            <Stack direction='row'>
+              <Avatar
+                sx={{
+                  bgcolor: blue[100],
+                  height: '120px',
+                  width: '120px',
+                  mt: 1,
+                }}
+                aria-label='resolve'
+              >
+                {profileData && (
+                  <img
+                    src={profileData.profilePicture}
+                    alt='profile-picture'
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover',
+                    }}
+                  />
+                )}
+              </Avatar>
+              <label htmlFor='profilePicture'>
+                <IconButton color='primary' component='span'>
+                  <EditIcon fontSize='md' />
+                </IconButton>
+              </label>
+            </Stack>
+            <Typography sx={{ fontWeight: 'medium', mt: 2 }}>
+              {profileData && profileData.firstname
+                ? profileData.firstname + ' ' + profileData.lastname
+                : profileData.username}
             </Typography>
             <Typography>{profileData && profileData.email}</Typography>
           </Box>
@@ -147,26 +185,26 @@ function UpdateProfile() {
           <Paper>
             <form onSubmit={handleSubmit}>
               <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
+                display='flex'
+                flexDirection='column'
+                alignItems='center'
                 p={2}
               >
                 <Typography
-                  variant="body1"
-                  color="#034f84"
+                  variant='body1'
+                  color='#034f84'
                   sx={{
-                    textAlign: "center",
-                    fontWeight: "bold",
+                    textAlign: 'center',
+                    fontWeight: 'bold',
                   }}
                 >
                   Profile settings
                 </Typography>
                 <TextField
-                  id="firstname"
-                  label={profileData.firstname==="" ? "Firstname" : ""}
-                  type="text"
-                  size="small"
+                  id='firstname'
+                  label={profileData.firstname === '' ? 'Firstname' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.firstname}
@@ -174,10 +212,10 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="lastname"
-                  label={profileData.firstname==="" ? "Lastname" : ""}
-                  type="text"
-                  size="small"
+                  id='lastname'
+                  label={profileData.firstname === '' ? 'Lastname' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.lastname}
@@ -185,10 +223,10 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="age"
-                  label={profileData.age==="" ? "Age" : ""}
-                  type="text"
-                  size="small"
+                  id='age'
+                  label={profileData.age === '' ? 'Age' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.age}
@@ -196,10 +234,10 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="gender"
-                  label={profileData.gender==="" ? "Gender" : ""}
-                  type="text"
-                  size="small"
+                  id='gender'
+                  label={profileData.gender === '' ? 'Gender' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.gender}
@@ -207,10 +245,10 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="education"
-                  label={profileData.education==="" ? "Education" : ""}
-                  type="text"
-                  size="small"
+                  id='education'
+                  label={profileData.education === '' ? 'Education' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.education}
@@ -218,10 +256,10 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="profession"
-                  label={profileData.profession==="" ? "Profession" : ""}
-                  type="text"
-                  size="small"
+                  id='profession'
+                  label={profileData.profession === '' ? 'Profession' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.profession}
@@ -229,18 +267,18 @@ function UpdateProfile() {
                 />
 
                 <TextField
-                  id="country"
-                  label={profileData.country==="" ? "Country" : ""}
-                  type="text"
-                  size="small"
+                  id='country'
+                  label={profileData.country === '' ? 'Country' : ''}
+                  type='text'
+                  size='small'
                   required
                   onChange={handleChange}
                   value={profileData.country}
                   sx={{ mb: 2 }}
                 />
 
-                <Button type="submit" variant="contained" sx={{ m: 1 }}>
-                  {" "}
+                <Button type='submit' variant='contained' sx={{ m: 1 }}>
+                  {' '}
                   save
                 </Button>
               </Box>
@@ -251,38 +289,40 @@ function UpdateProfile() {
         <Grid item xs={12} md={4} lg={4}>
           <Paper>
             <form onSubmit={handleSubmit}>
-            <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
+              <Box
+                display='flex'
+                flexDirection='column'
+                alignItems='center'
                 p={2}
               >
                 <Typography
-                  variant="body1"
-                  color="#034f84"
+                  variant='body1'
+                  color='#034f84'
                   sx={{
-                    textAlign: "center",
-                    fontWeight: "bold",
+                    textAlign: 'center',
+                    fontWeight: 'bold',
                   }}
                 >
                   Add your interests
                 </Typography>
-               
+
                 {dataTags.tags.map((tagObject, index) => {
                   const tagLabel = Object.values(tagObject)[0];
                   return (
                     <Chip
                       key={index}
                       label={tagLabel}
-                      id="category"
+                      id='category'
                       onClick={() => handleClick(tagLabel)}
-                      color={selectedTags.includes(tagLabel) ? 'primary' : 'default'}
+                      color={
+                        selectedTags.includes(tagLabel) ? 'primary' : 'default'
+                      }
                     />
                   );
                 })}
 
-                <Button type="submit" variant="contained" sx={{ m: 1 }}>
-                  {" "}
+                <Button type='submit' variant='contained' sx={{ m: 1 }}>
+                  {' '}
                   save
                 </Button>
               </Box>
