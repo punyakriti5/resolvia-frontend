20333a355f7744bfc0e554a121bccbbabb0f6c79
Abishek sridhar <abishekcs3896@gmail.com>
2024-02-17 04:28:44 +0530

    Refresh Token not saved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
-import { Box, Container, Stack, Typography } from "@mui/material";
-import question from "../assets/query.jpg";
-import ask from "../assets/discussions.jpg";
-import resolution from "../assets/resolution.jpg";
-import { Navigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { signoutSuccess } from "../features/user/userSlice";
-import Navbar from "../components/Navbar";
-import FooterComp from "../components/FooterComp";
+import { useState, useEffect } from 'react';
+import { Box, Container, Stack, Typography } from '@mui/material';
+import question from '../assets/query.jpg';
+import ask from '../assets/discussions.jpg';
+import resolution from '../assets/resolution.jpg';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { signoutSuccess } from '../features/user/userSlice';
+import Navbar from '../components/Navbar';
+import FooterComp from '../components/FooterComp';
 
-import { BASE_API_URL } from "../constants";
-import TypingEffect from "../components/TypingEffect";
+import { BASE_API_URL } from '../constants';
+import TypingEffect from '../components/TypingEffect';
 
 function Home() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector(state => state.user);
   const dispatch = useDispatch();
   const [isCurrentUser, setCurrentUser] = useState(false);
-  const token = sessionStorage.getItem("token");
+  const token = sessionStorage.getItem('token');
   useEffect(() => {
     async function fetchUser() {
       if (currentUser) {
         try {
           const res = await fetch(`${BASE_API_URL}/api/auth/refresh`, {
-            method: "POST",
+            method: 'POST',
 
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
               Authorization: `Bearer ${token}`,
             },
             body: JSON.stringify(currentUser),
@@ -34,6 +34,7 @@ function Home() {
           if (!res.ok) {
             dispatch(signoutSuccess());
           } else {
+            sessionStorage.setItem('token', data.accessToken);
             setCurrentUser(true);
           }
         } catch (error) {
@@ -57,33 +58,33 @@ function Home() {
           <Navbar />
 
           <Box
-            component="div"
+            component='div'
             sx={{
               mt: 8,
-              width: "100%",
+              width: '100%',
               p: 5,
-              background: "#bed8ec",
-              display: "flex",
+              background: '#bed8ec',
+              display: 'flex',
             }}
           >
             <Stack
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
+              direction='column'
+              justifyContent='center'
+              alignItems='center'
               spacing={2}
             >
               <Typography
-                variant="h4"
-                color="#034f84"
-                fontWeight={"bold"}
+                variant='h4'
+                color='#034f84'
+                fontWeight={'bold'}
                 px={5}
               >
                 Welcome to Resolvia
               </Typography>
               <Typography
-                variant="body1"
-                color="#034f84"
-                fontWeight={"bold"}
+                variant='body1'
+                color='#034f84'
+                fontWeight={'bold'}
                 px={5}
               >
                 where connectivity meets innovation and the future of technical
@@ -92,40 +93,40 @@ function Home() {
             </Stack>
 
             <Stack
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
+              direction='row'
+              justifyContent='center'
+              alignItems='center'
               spacing={2}
             >
               <Box
-                component="img"
-                alt="Have query ?"
+                component='img'
+                alt='Have query ?'
                 src={question}
                 sx={{
                   height: 135,
                   width: 250,
                   borderRadius: 5,
                   boxShadow: 5,
-                  display: { md: "flex", lg: "flex", xs: "none" },
+                  display: { md: 'flex', lg: 'flex', xs: 'none' },
                 }}
               />
 
               <Box
-                component="img"
-                alt=" Ask here..."
+                component='img'
+                alt=' Ask here...'
                 src={ask}
                 sx={{
                   height: 225,
                   width: 300,
                   borderRadius: 5,
                   boxShadow: 5,
-                  display: { md: "flex", lg: "flex", xs: "none" },
+                  display: { md: 'flex', lg: 'flex', xs: 'none' },
                 }}
               />
 
               <Box
-                component="img"
-                alt="Get answer"
+                component='img'
+                alt='Get answer'
                 src={resolution}
                 sx={{ height: 300, width: 325, borderRadius: 5, boxShadow: 5 }}
               />
@@ -133,16 +134,16 @@ function Home() {
           </Box>
 
           <Typography
-            variant="h5"
+            variant='h5'
             sx={{
-              color: "#034f84",
-              px: "26%",
-              pb:8,
-              background: "#bed8ec",
-              display: "flex",
+              color: '#034f84',
+              px: '26%',
+              pb: 8,
+              background: '#bed8ec',
+              display: 'flex',
             }}
           >
-            <TypingEffect text="Have a query? -> Ask to Resolvia  -> Get it resolved ! " />
+            <TypingEffect text='Have a query? -> Ask to Resolvia  -> Get it resolved ! ' />
           </Typography>
 
           <FooterComp />
